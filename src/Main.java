cc9337bb5ae9c785ac77438b07b636e7254616be
Claudio Neves <cfneguacu@gmail.com>
2026-04-21 21:56:52 -0300

    Fazendo ajustes no codigo.

--- src/Main.java
+++ src/Main.java
@@ -6,20 +6,15 @@ public class Main {
             int tamanho = 9;
             int[][] vetor = new int[tamanho][tamanho];
             StringBuilder row = new StringBuilder();
-            String[] t = new String[tamanho];
             for(int i = 0;i < vetor[0].length-1;i++){
                 vetor[i][0] = 1;
                 row.append(vetor[i][0]).append(" ");
-                //System.out.print(vetor[i][0]+" ");
                 for(int j = 0; j < i ;j++) {
                     vetor[i + 1][j + 1] = vetor[i][j] + vetor[i][j + 1];
                     row.append(vetor[i + 1][j + 1]).append(" ");
                 }
-                t[i] = row.toString();
+                System.out.print(row.toString().indent((tamanho-i)));
                 row = new StringBuilder();
-                System.out.println(t[i]);
-                //System.out.print("-> "+multiply+"\n");
-                //multiply = 1;
         }
 
     }
